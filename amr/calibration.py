791b8b7b3c6641ce29174cfcc2b553dd0ee50290
Shashank Ghosh <103781989+iLUVE69@users.noreply.github.com>
2024-11-22 19:37:31 +0530

    Update calibration.py

--- amr/calibration.py
+++ amr/calibration.py
@@ -30,11 +30,9 @@ def rescaleROI(src, roi):
 file_list = [i for i in listdir(join(ROOT, 'L'))]
 file_list.sort(key=sort_id)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((7*7,3), np.float32)
 objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
 
-# Store computed points
 objPts = []
 imgPL, imgPR = [], []
 
@@ -54,29 +52,16 @@ for f in file_list:
 
     if retL and retR:
         objPts.append(objp)
-        corners2L = cv.cornerSubPix(grayL, cornersL, (5,5), (-1,-1), criteria_calib)
+        corners2L = cv.cornerSubPix(grayL, cornersL, (3,3), (-1,-1), criteria_calib)
         imgPL.append(corners2L)
         # objPR.append(objp)
-        corners2R = cv.cornerSubPix(grayR, cornersR, (5,5), (-1,-1), criteria_calib)
+        corners2R = cv.cornerSubPix(grayR, cornersR, (3,3), (-1,-1), criteria_calib)
         imgPR.append(corners2R)
 
-        ### Draw and output file of detected points
-        # if f == output_id:
-        #     cv.imwrite(r'L_' + fname, imgL)
-        #     cv.drawChessboardCorners(imgL, (9,6), corners2L, retL)
-        #     cv.imwrite(r'GRID_L_' + fname, imgL)
-
-        #     cv.imwrite(r'R_' + fname, imgR)
-        #     cv.drawChessboardCorners(imgR, (9,6), corners2R, retR)
-        #     cv.imwrite(r'GRID_R_' + fname, imgR)
-        #     cv.waitKey(500)
-        #     print(f'OUTPUT: {f}')
-        #     exit()
     else:
         print(f'Corners not found in {fname} (Left={retL}, Right={retR})')
         break
 
-#F, mask = cv.findFundamentalMat(np.float32(imgPL), np.float32(imgPR), cv.FM_8POINT)
 objPts = np.asarray(objPts, np.float32)
 imgPL = np.asarray(imgPL, np.float32)
 imgPR = np.asarray(imgPR, np.float32)
@@ -91,8 +76,6 @@ mse2, C2, D2, R2, T2 = cv.calibrateCamera(objPts, imgPR, imgSize,
 mseTotal,CL,DL,CR,DR,R,T,E,F = cv.stereoCalibrate(objPts, imgPL, imgPR, 
                         C1, D1, C2, D2, imgSize, flags=flags_stereo_calib, criteria=criteria_calib)
 
-# retval,CLl,DLl,CR,DR,rr,tt,E,F,pve = cv.stereoCalibrateExtended(objPts, imgPL, imgPR, 
-#                                     C1, D1, C2, D2, imgSize, R, T, flags=flags_stereo_calib, criteria=criteria_calib)                                
 
 ### Rectification Transforms, Projection Matrices, ROIs after rectification
 RL,RR,PL,PR,Q,validROIL,validROIR = cv.stereoRectify(CL, DL, CR, DR, imgSize, R, T, alpha=0, 
@@ -112,39 +95,7 @@ print(f'Right MSE: {mse1:0.6f}')
 print(f'Overall MSE: {mseTotal:0.6f} ({len(file_list)} image pairs)\n')
 
 labels = []
-# for i,e in enumerate(pve):
-#     print(f'Pair {file_list[i]}: {e}')
-#     n = file_list[i].split('.')[0]
-#     labels.append(n)
 
-
-# ''' Display plot of per-view reprojection error '''
-# fig, ax = plt.subplots()
-# x_label_pos = np.arange(len(pve))
-# bar_width = 0.35
-# rects1 = ax.bar(x_label_pos - bar_width/2, pve[:,0], bar_width, label='Camera 1')
-# rects2 = ax.bar(x_label_pos + bar_width/2, pve[:,1], bar_width, label='Camera 2')
-# ax.axhline(retval, color="black", linestyle="--", label='Overall MSE='+str(retval)[:4])
-
-# ax.set_ylabel('Mean Squared Error (pixels)'); ax.set_xlabel('Image Pair ID')
-# ax.set_title('Pixel Reprojection Error per Image Pair')
-# ax.set_xticks(x_label_pos); ax.set_xticklabels(labels)
-# ax.legend()
-
-### Refine Rotational and Translational
-# rr, j = cv.Rodrigues(R)
-
-# pnp1, rv1, tv1 = cv.solvePnP(objPts, imgPL, C1, D1, flags=(cv.SOLVEPNP_SQPNP))
-# print('rv1 {}\ntv1 {}'.format(rv1, tv1))
-# print(pnp1)
-
-# pnp2, rv2, tv2, inliers = cv.solvePnPRansac(objPts[0], imgPL[0], C1, D1, rvec=rr, tvec=T, useExtrinsicGuess=True)
-# print('rv2 {}\ntv2 {}'.format(rv2, tv2))
-#print(pnp2)
-
-# rv3, tv3 = cv.solvePnPRefineLM(objPts[0], imgPL[0], C1, D1, rr, T)
-# print('rv3 {}\ntv3 {}'.format(rv3, tv3))
-#print(pnp2)
 
 
 ''' Rectification mapping '''
@@ -185,18 +136,14 @@ prompt = input('Save parameters to "{}\\"? (y/n): '.format(output_dir))
 if (prompt == 'y'):
     if not isdir(output_dir):
         mkdir(output_dir)
-    # np.savetxt(r'Calibration_Files\C1.txt', C1, fmt='%.5e')   # identical to CL
-    # np.savetxt(r'Calibration_Files\D1.txt', D1, fmt='%.5e')   # identical to DL
+
     np.savetxt(join(output_dir, 'Q.txt'), Q, fmt='%.5e')
-    # np.savetxt(join(output_dir, 'FundMat.txt'), F, fmt='%.5e')
     np.savetxt(join(output_dir, 'CmL.txt'), CL, fmt='%.5e')
     np.savetxt(join(output_dir, 'CmR.txt'), CR, fmt='%.5e')
     np.savetxt(join(output_dir, 'DcL.txt'), DL, fmt='%.5e')
     np.savetxt(join(output_dir, 'DcR.txt'), DR, fmt='%.5e')
     np.savetxt(join(output_dir, 'Rtn.txt'), R, fmt='%.5e')
     np.savetxt(join(output_dir, 'Trnsl.txt'), T, fmt='%.5e')
-    # np.savetxt(join(output_dir, 'RtnL.txt'), R1, fmt='%.5e')  # Contains 'n' estimate arrays from 'n' images
-    # np.savetxt(join(output_dir, 'TrnslL.txt'), T1, fmt='%.5e')
     np.savetxt(join(output_dir, 'RectifL.txt'), RL, fmt='%.5e')
     np.savetxt(join(output_dir, 'ProjL.txt'), PL, fmt='%.5e')
     np.savetxt(join(output_dir, 'ProjR.txt'), PR, fmt='%.5e')
